35e95782fa3269dadd90125591d7890621a974f3
ShlomoCode <78599753+ShlomoCode@users.noreply.github.com>
2026-09-27 01:33:17 +0300

    fix(macos): use regular startup cache cleanup

--- app/lib/config/init.dart
+++ app/lib/config/init.dart
@@ -290,11 +290,7 @@ Future<void> postInit(BuildContext context, Ref ref, bool appStart) async {
   if (appStart && !hasInitialShare && (checkPlatformWithGallery() || checkPlatformCanReceiveShareIntent() || checkPlatform([TargetPlatform.macOS]))) {
     // Clear cache on every app start.
     // If we received a share intent, then don't clear it, otherwise the shared file will be lost.
-    if (checkPlatform([TargetPlatform.macOS])) {
-      await ref.global.dispatchAsync(ClearCacheAction());
-    } else {
-      ref.global.dispatchAsync(ClearCacheAction()); // ignore: unawaited_futures
-    }
+    ref.global.dispatchAsync(ClearCacheAction()); // ignore: unawaited_futures
   }
 
   if (!ref.read(persistenceProvider).isFirstAppStart) {
